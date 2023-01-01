6e395d3b04935fd74d4754f164266a1ebecc87ac
Kamil <k.sachryn@gmail.com>
2023-01-01 00:46:15 -0500

    comment program

--- KamiTrix/Program.cs
+++ KamiTrix/Program.cs
@@ -2,115 +2,92 @@
 using System.Diagnostics;
 using System.Drawing;
 
-const int height = 20;
-const int width = 100;
-const int maxLength = 16;
-Random random = new Random();
-Grid grid = new Grid(height, width);
-StatusGrid status = new StatusGrid(maxLength);
-
-Grid _oldGrid = new Grid(height, width);
-StatusGrid _oldStatus = new StatusGrid(maxLength);
-
+//default settings for unmodified windows 11 console
+const int CONSOLE_HEIGHT = 20;
+const int CONSOLE_WIDTH = 100;
+const int MAX_TRAIL_LENGTH = 16;
 const int MS_REPEAT = 70;
-const int PICK_NEW_HIGHLIGHT = 25;
-
-grid.randomizeGrid();
+const int MS_TICK_NEW_HIGHLIGHT = 25;
 
 ConsoleColor _default = ConsoleColor.Black;
 ConsoleColor _black = ConsoleColor.Black;
-ConsoleColor _darkBlue = ConsoleColor.DarkBlue;
 ConsoleColor _darkGreen = ConsoleColor.DarkGreen;
-ConsoleColor _darkCyan = ConsoleColor.DarkCyan;
-ConsoleColor _darkRed = ConsoleColor.DarkRed;
-ConsoleColor _darkMagenta = ConsoleColor.DarkMagenta;
-ConsoleColor _darkYellow = ConsoleColor.DarkYellow;
-ConsoleColor _gray = ConsoleColor.Gray;
-ConsoleColor _darkGray = ConsoleColor.DarkGray;
 ConsoleColor _blue = ConsoleColor.Blue;
-ConsoleColor _green = ConsoleColor.Green;
-ConsoleColor _cyan = ConsoleColor.Cyan;
-ConsoleColor _red = ConsoleColor.Red;
-ConsoleColor _magenta = ConsoleColor.Magenta;
-ConsoleColor _yellow = ConsoleColor.Yellow;
-ConsoleColor _white = ConsoleColor.White;
-
-//ConsoleColor[] _colors = { _black, _darkBlue, _darkGreen, _darkCyan, _darkRed, _darkMagenta, _darkYellow, _gray, _darkGray, _blue, _green, _cyan, _red, _magenta, _yellow};
-ConsoleColor[] _colors = { _black, _darkGreen, _green };
 
 Console.BackgroundColor = ConsoleColor.Black;
 Console.CursorVisible = false;
 
+//tick cycle
+Stopwatch stopwatchTick = Stopwatch.StartNew();
+Stopwatch stopwatchNewTrail = Stopwatch.StartNew();
+
+//object representations
+Grid grid = new Grid(CONSOLE_HEIGHT, CONSOLE_WIDTH);
+StatusGrid status = new StatusGrid(MAX_TRAIL_LENGTH);
+
+//tick to tick change for optimization
+//trade memory for speed
+Grid _oldGrid = new Grid(CONSOLE_HEIGHT, CONSOLE_WIDTH);
+StatusGrid _oldStatus = new StatusGrid(MAX_TRAIL_LENGTH);
 
 
+//Initial console setup
+grid.randomizeGrid();
 
-Stopwatch stopwatch = Stopwatch.StartNew();
+
+Random random = new Random();
+
+
+//initial setup at ms == 0
 bool init = true;
-
-Stopwatch stopwatch1 = Stopwatch.StartNew();
-
 
 while (true)
 {
-    if (stopwatch.ElapsedMilliseconds >= MS_REPEAT || init)
+    //refresh console
+    if (stopwatchTick.ElapsedMilliseconds >= MS_REPEAT || init)
     {
         init = false;
         Console.Clear();
         grid.randomizeGrid();
         printCharacters(grid.getCharArr());
         status.tickColors();
-        stopwatch.Restart();
 
+        stopwatchTick.Restart();
     }
 
-    if (stopwatch1.ElapsedMilliseconds >= PICK_NEW_HIGHLIGHT)
+    //start new trail
+    if (stopwatchNewTrail.ElapsedMilliseconds >= MS_TICK_NEW_HIGHLIGHT)
     {
         (int tempx, int tempy) = status.getRandomPosition();
+        status.statusGrid[tempx][tempy] = MAX_TRAIL_LENGTH;
 
-        status.statusGrid[tempx][tempy] = maxLength;
-
-
-        stopwatch1.Restart();
+        stopwatchNewTrail.Restart();
     }
 }
 
+//reads through the grids to check for changed characters and colors,
+//writes new character directly to position
+
 void printCharacters(char[,] characters)
 {
-    /*
-    for (int i = 0; i < height; i++)
+    for (int i = 0; i < CONSOLE_HEIGHT; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < CONSOLE_WIDTH; j++)
         {
-            if (status.statusGrid[i][ j] != 0)
-            {
-                
-                int tmp = status.statusGrid[i][ j];
-                Console.ForegroundColor = _colors[tmp];
-            }
-            Console.Write(grid.grid[i][j]);
-            Console.ForegroundColor = _default;
-        }
-        Console.WriteLine();
-    }
-    */
-
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-
-
+            //if a character is changed
             if (_oldGrid.grid[i][j] != grid.grid[i][j])
             {
+                //if it is colored
                 if (status.statusGrid[i][j] != 0)
                 {
-
-
+                    //set current character to desired color
+                    //blue for trail head
+                    //green for tail
                     int tmp = status.statusGrid[i][j];
 
                     Console.SetCursorPosition(j, i);
 
-                    if (tmp == maxLength)
+                    if (tmp == MAX_TRAIL_LENGTH)
                     {
                         Console.ForegroundColor = _blue;
                     }
@@ -118,7 +95,6 @@ void printCharacters(char[,] characters)
                     {
                         Console.ForegroundColor = _darkGreen;
                     }
-
                     else
                     {
                         Console.ForegroundColor = _black;
@@ -133,19 +109,10 @@ void printCharacters(char[,] characters)
 
     }
 
-
-
-    copyGrid();
-
-
-}
-
-void copyGrid()
-{
-
-    for (int i = 0; i < height; i++)
+    //copy grids without assignment to oldgrids
+    for (int i = 0; i < CONSOLE_HEIGHT; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < CONSOLE_WIDTH; j++)
         {
             _oldGrid.grid[i][j] = grid.grid[i][j];
             _oldStatus.statusGrid[i][j] = status.statusGrid[i][j];
